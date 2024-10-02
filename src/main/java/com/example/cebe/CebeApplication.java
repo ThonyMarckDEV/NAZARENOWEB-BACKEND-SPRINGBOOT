d3e41c6d3db91062ab1ea318b5841f6f4f580073
ThonyMarckDEV <thonymarckyt08@gmail.com>
2024-10-01 23:20:35 -0500

    FIRST COMMIT JAVA

---
+++ src/main/java/com/example/cebe/CebeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CebeApplication.class, args);
+	}
+
+}
